f3fd10afdf03a435aa84b42eedcc26900aeb0a0b
codebender828 <codebender828@gmail.com>
2024-07-26 15:28:22 +0300

    feat: added airdorp script

--- ts/src/keypair.ts
+++ ts/src/keypair.ts
@@ -1,8 +1,7 @@
 import consola from "consola";
-import { generateKeyPair } from "@solana/web3.js";
+import { generateKeyPair, address } from "@solana/web3.js";
 
 const logger = consola.withTag("keypair");
 
 const keypair = await generateKeyPair();
 logger.info("keypair secret key", keypair.privateKey);
-logger.info("keypair address", keypair.publicKey);
